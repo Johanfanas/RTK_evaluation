b2857a05d95d735b6636991c9f1d64c857e0a52c
Johan Fanas <62773660+Johanfanas@users.noreply.github.com>
2022-03-09 00:12:22 -0500

    Add files via upload
    Finished script and uploaded yaml file

--- create_csv.py
+++ create_csv.py
@@ -1,26 +1,78 @@
 #!/usr/bin/env python3
 
+from itertools import zip_longest
 import csv
+import yaml
 import rostopic
 import rospy
+import argparse
 
-# def topic_handler(topic_name, message_type):
+class SUBHANDLER(object):
 
-#     rospy.Subscriber( topic_name, message_type )
+    def __init__(self, config_file, filename):
 
-topics = int( input("Enter the number of topics you want to save into csv: ") )
+        self.filename = filename
 
-names, col_names = [], []
+        self.variables = {}
 
-for i in range(topics):
+        for topic in config_file:
+            for name in config_file[topic]['column_names']:
+                self.variables[config_file[topic]['column_names'][name]] = []
 
-    names.append( input( "Enter name of topic No.{}: ".format(i) ) )
-    col_names.append( input( "Column name in csv for topic No.{}: ".format(i) ) )
-    # data = input( "Data to extract: " )
-    # print("")
+        for topic in config_file:
+            message_type = rostopic.get_topic_class(config_file[topic]['name'])[0]
 
-msg = rostopic.get_topic_class(names[0])[0]
+            rospy.Subscriber(config_file[topic]['name'], message_type,
+                callback=self.callback_handler, callback_args=(config_file[topic]))
 
-rospy.init_node( 'node' )
-m = rospy.wait_for_message( names[0], msg)
-print(m.__slots__)
+    def callback_handler(self, data, args):
+        
+        for col_name, param in zip(args['column_names'], args['parameters']):
+
+            msg = data
+
+            for s in args['parameters'][param].split('.'):
+                msg = getattr(msg, s)
+
+            self.variables[args['column_names'][col_name]].append( msg ) 
+
+    def __del__(self):
+
+        path = self.filename + '.csv'
+
+        print('\033[92m' + "Writing csv file ", path)
+
+        fields = self.variables.keys()
+
+        # writing to csv file
+        with open( path ,"w+") as f:
+            # creating a csv writer object
+            csvwriter = csv.writer(f)
+
+            # writing the fields 
+            csvwriter.writerow(fields)
+
+            for values in zip_longest(*self.variables.values()):
+                csvwriter.writerow(values)
+
+parser = argparse.ArgumentParser(description='ROS to CSV')
+parser.add_argument('--filename', default='output', help='Name of output csv file')
+
+args = parser.parse_args()
+
+try:
+
+    with open('config.yaml', 'r') as file:
+        params = yaml.safe_load(file)
+
+    rospy.init_node('csv_node')
+    obj = SUBHANDLER(params, args.filename)
+    rospy.spin()
+except KeyboardInterrupt:
+    pass
+
+'''
+    Forces python to delete the object before the python interperter finishes
+    to guarantee creating csv file
+'''
+del obj
